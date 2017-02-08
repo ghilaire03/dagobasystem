66803c6876014aa57de06cbecdb6b2aa2ad1c8f0
greg <ghilaire03@gmail.com>
2017-02-07 20:12:31 -0500

    yeaa

---
+++ jakku/helloworld.py
@@ -0,0 +1,22 @@
+print ("hello world")
+'''hey lets party'''
+print ("lets get down lets party")
+Test1=float(input("please input the score for test 1. "))
+Test2=float(input("please input the score for test 2. "))
+Test3=float(input("please input the score for test 3. "))
+average= (Test1+Test2+Test3)/3
+print("the average of all 3 test is ",average)
+
+
+print(5.0/9.0)
+print(5.0/9)
+print(5/9.0)
+print(5/9)
+print(9.0/5.0)
+print(9.0/5)
+print(9/5.0)
+print(9/5)
+
+print("my name is ",end=" ")
+print("greg")
+
---
+++ jakku/rando.py
@@ -0,0 +1,8 @@
+temperature=float(input("please input temperature "))
+if temperature <60:
+    print ("brick")
+elif temperature >90:
+    print ("damn nigga its hot af")
+else:
+    print ("perf where da panties")
+
